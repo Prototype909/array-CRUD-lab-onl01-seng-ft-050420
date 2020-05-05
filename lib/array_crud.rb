e22d5204b011b203e6616bbd5fa5fd62de86156e
Steve Anderson <prototype909@msn.com>
2020-05-05 23:47:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_cats = ["Tiger", "Princess", "Cowtown", "Will"]
-  my_cats << "Nala"
+  my_cats.push("Nala")
 end
 
 def add_element_to_start_of_array(array, element)
