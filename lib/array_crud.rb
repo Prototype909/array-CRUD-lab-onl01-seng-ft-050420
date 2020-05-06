324a77b68ebb5aaca0cc0c98a7cdb4e45f10f66f
Steve Anderson <prototype909@msn.com>
2020-05-06 01:03:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_cats = ["wow", "I", "am", "really", "learning"]
-  my_cats[1]
+  my_cats[2]
 end
 
 def retrieve_first_element_from_array(array)
