348ecd8095f2e6782b1a95eb5fd06e39690ebd10
Steve Anderson <prototype909@msn.com>
2020-05-06 00:56:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  my_cats = ["Tiger", "Princess", "Cowtown", "arrays!"]
+  my_cats = ["wow", "Princess", "Cowtown", "arrays!"]
   my_cats.shift
 end
 
