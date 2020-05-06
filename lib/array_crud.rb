695172048c20af9026bc60e8c72f69d73b33cc35
Steve Anderson <prototype909@msn.com>
2020-05-06 00:50:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_cats = ["Tiger", "Princess", "Cowtown", "arrays!"]
-
+  "arrays!" = my_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
