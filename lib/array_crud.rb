592b114a2f3c40626e27c9a4dcf982c2e10cd60f
Steve Anderson <prototype909@msn.com>
2020-05-06 00:52:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_cats = ["Tiger", "Princess", "Cowtown", "Will"]
+  my_cats = ["Tiger", "Princess", "Cowtown", "Will",]
   my_cats.unshift("wow")
 end
 
