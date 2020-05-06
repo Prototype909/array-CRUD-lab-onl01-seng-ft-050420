483d8e0f61417a3c1b3315644c67e51c9e2fbc78
Steve Anderson <prototype909@msn.com>
2020-05-06 01:00:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  my_cats = ["wow", "I", "am", "really", "learning"]
-  my_cats[3]
+
 end
 
 def retrieve_first_element_from_array(array)
