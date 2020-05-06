322cb41dfae779a91444c465767e4328ee3b4eea
Steve Anderson <prototype909@msn.com>
2020-05-06 00:55:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  my_cats = ["Tiger", "Princess", "Cowtown", "arrays!"]
+  my_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
