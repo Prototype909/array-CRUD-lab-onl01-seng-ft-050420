dd0d33ebd3367af9d6956584f68715a7b6db7601
Steve Anderson <prototype909@msn.com>
2020-05-06 00:58:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_cats = ["Tiger", "Princess", "Cowtown", "Will"]
+  my_cats = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
